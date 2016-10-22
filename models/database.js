8ac4d299e512285d78b77a34f796ea18d5c0e87d
ruben soerdien <ruben soerdien>
2016-10-22 04:57:26 +0200

    fix5sd

--- models/database.js
+++ models/database.js
@@ -1,10 +1,10 @@
-//var pg = require('pg');
+var pg = require('pg');
 
 // create a config to configure both pooling behavior 
 // and client options 
 // note: all config is optional and the environment variables 
 // will be read if the config is not present 
-/*var config = {
+var config = {
     host:process.env.host,
     user: process.env.user, //env var: PGUSER
     database: process.env.database, //env var: PGDATABASE
@@ -18,11 +18,12 @@
 //this initializes a connection pool
 //it will keep idle connections open for a 30 seconds
 //and set a limit of maximum 10 idle clients
-var pool = new pg.Pool(config);*/
-var pg = require('pg').native
-pg.connect(process.env.databaseURL, function (err, conn, done) {
-    if (err) throw err;
+var pool = new pg.Pool(config);
+pool.connect(function (err, client, done) {
+    if (err) {
+        console.log('error fetching client from pool', err);
 
+    }
     else {
         client.query('CREATE TABLE IF NOT EXISTS Glitch(id SERIAL PRIMARY KEY,idImage INT, text VARCHAR(40) not null)',
             function (err, result) {
@@ -50,7 +51,7 @@ pg.connect(process.env.databaseURL, function (err, conn, done) {
 // to run a query we can acquire a client from the pool,
 // run a query on the client, and then return the client to the pool
 var connection = function (callback) {
-    pg.connect(process.env.DATABASE_URL ,function (err, client, done) {
+    pool.connect(function (err, client, done) {
         if (err) {
              console.log('error fetching client from pool', err);
             return callback(err, null);
@@ -70,7 +71,7 @@ var connection = function (callback) {
         });*/
     });
 };
-/*pool.on('error', function (err, client) {
+pool.on('error', function (err, client) {
     // if an error is encountered by a client while it sits idle in the pool
     // the pool itself will emit an error event with both the error and
     // the client which emitted the original error
@@ -78,6 +79,6 @@ var connection = function (callback) {
     // between your application and the database, the database restarts, etc.
     // and so you might want to handle it and at least log it out
     console.error('idle client error', err.message, err.stack)
-});*/
+});
 
 module.exports = connection;
