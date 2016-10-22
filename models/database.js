80163ba40c9624d6dd35aca2ec44b59338e388ff
ruben soerdien <ruben soerdien>
2016-10-22 04:32:45 +0200

    fix2

--- models/database.js
+++ models/database.js
@@ -52,7 +52,7 @@ pg.connect(process.env.DATABASE_URL ,function (err, client, done) {
 // to run a query we can acquire a client from the pool,
 // run a query on the client, and then return the client to the pool
 var connection = function (callback) {
-    pool.connect(function (err, client, done) {
+    pg.connect(process.env.DATABASE_URL ,function (err, client, done) {
         if (err) {
              console.log('error fetching client from pool', err);
             return callback(err, null);
@@ -72,7 +72,7 @@ var connection = function (callback) {
         });*/
     });
 };
-pool.on('error', function (err, client) {
+/*pool.on('error', function (err, client) {
     // if an error is encountered by a client while it sits idle in the pool
     // the pool itself will emit an error event with both the error and
     // the client which emitted the original error
@@ -80,6 +80,6 @@ pool.on('error', function (err, client) {
     // between your application and the database, the database restarts, etc.
     // and so you might want to handle it and at least log it out
     console.error('idle client error', err.message, err.stack)
-});
+});*/
 
 module.exports = connection;
