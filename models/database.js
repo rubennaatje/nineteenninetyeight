d5a821de03cf7d0613662aeac34a0c0edb6f57b2
ruben soerdien <ruben soerdien>
2016-11-06 22:43:57 +0100

    'haha'

--- models/database.js
+++ models/database.js
@@ -1,4 +1,25 @@
+/*var pg = require('pg');
 
+// create a config to configure both pooling behavior 
+// and client options 
+// note: all config is optional and the environment variables 
+// will be read if the config is not present 
+var config = {
+    URL: process.env.DATABASE_URL,
+    host:process.env.host,
+    user: process.env.user, //env var: PGUSER
+    database: process.env.database, //env var: PGDATABASE
+    password: process.env.password, //env var: PGPASSWORD
+    port: 5432, //env var: PGPORT
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+};
+
+
+//this initializes a connection pool
+//it will keep idle connections open for a 30 seconds
+//and set a limit of maximum 10 idle clients
+var pool = new pg.Pool(config);*/
 var pg = require('pg');
 
 // instantiate a new client
@@ -35,6 +56,17 @@ client.connect(function (err, client, done) {
 });
 // to run a query we can acquire a client from the pool,
 // run a query on the client, and then return the client to the pool
+var connection = function (callback) {
+    client.connect(function (err, client, done) {
+         if (err) {
+             console.log('error fetching client from pool', err);
+            return callback(err, null);
+        }
+        else{
+            return callback(null, client);
+        }
+    });
+};
 client.on('error', function (err, client) {
     // if an error is encountered by a client while it sits idle in the pool
     // the pool itself will emit an error event with both the error and
